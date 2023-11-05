43d1a97924f67ec61d029211a17936d6985fcc95
Vaishnavi Gupta <113535692+Vaishnavi2445@users.noreply.github.com>
2023-11-06 00:38:12 +0530

    Create DFSTreeTraversal.cpp

---
+++ VS Code/DFSTreeTraversal.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+  int data;
+  node *left;
+  node *right;
+
+
+
+  node(int data) {
+    this->data = data;
+    this->left = nullptr;
+    this->right = nullptr;
+  }
+
+ 
+};
+
+node *tree() {
+  int data;
+
+  cout << "enter Data : " << endl;
+  cin >> data;
+
+  if (data == -1) {
+    return NULL;
+  }
+
+  // Create a new node with the given data.
+  node *root = new node(data);
+
+  cout << "Enter left data for " << data << endl;
+
+  root->left = tree();
+
+  cout << "Enter right data for " << data << endl;
+
+  root->right = tree();
+
+  return root;
+}
+
+void printPreOrder(node *root) {
+  if (root != nullptr) {
+    cout << root->data << " ";
+    printPreOrder(root->left);
+    printPreOrder(root->right);
+  }
+
+}
+
+void printInOrder(node *root) {
+  if (root != nullptr) {
+    
+    printPreOrder(root->left);
+    cout << root->data << " ";
+    printPreOrder(root->right);
+  }
+  
+}
+void printPostOrder(node *root) {
+  if (root != nullptr) {
+    
+    printPreOrder(root->left);
+    printPreOrder(root->right);
+    cout << root->data << " ";
+  }
+}
+
+
+int main() {
+  node *root = tree();
+  printPreOrder(root);
+  cout<<endl;
+  printInOrder(root);
+  cout<<endl;
+  printPostOrder(root);
+  return 0;
+}
