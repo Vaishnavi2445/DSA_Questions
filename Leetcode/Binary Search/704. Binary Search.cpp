b0f0c0a37a312118f4f1c07cbaf3efc8c12d768f
Vaishnavi Gupta <113535692+Vaishnavi2445@users.noreply.github.com>
2024-01-16 03:28:23 +0530

    Create 704. Binary Search.cpp

---
+++ Leetcode/Binary Search/704. Binary Search.cpp
@@ -0,0 +1,40 @@
+
+Linear Search:
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for(int i=0; i<nums.size(); i++){
+             if(nums[i]==target){
+                return i;
+            }
+        
+            
+        }
+        return -1;
+    }
+};
+
+Binary Search:
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return -1;
+    }
+};
+
+
